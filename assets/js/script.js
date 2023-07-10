b9266a94df78355c319c723d89430b7385c5a9ce
MattuW4 <>
2023-07-10 12:02:33 +0000

    Add declare section for global variables. Add event listener for player choice. Add generate random computer weapon. Add function for player weapon choice. Begin to add function display outcome and update scores

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,49 @@
+// Declare global variables
+const choices = document.querySelectorAll(".player-weapon-choice-button");
+const playerChoice = document.querySelector("#playerChoice");
+const computerChoiceOutput = document.querySelector(".computer-choice-output")
+
+const weaponOptions = ["rock", "paper", "scissors"];
+let = playerScore = 0;
+let computerScore = 0;
+let roundNumber = 0;
+
+// Event listener for player weapon choice
+choices.forEach((choice) => choice.addEventListener("click", weaponChoice));
+
+// Generate random computer weapon
+function randomComputer() {
+    let index = Math.floor(Math.random() * weaponOptions.length);
+    return weaponOptions[index];
+}
+
+// Function for player weapon choice
+function weaponChoice() {
+    playerChoice.textContent = choices;
+    let playerWeapon = this.id;
+    let computerWeapon = randomComputer();
+    updateScore(playerWeapon, computerWeapon);
+}
+
+// Function to display outcome and update scores
+function scoreUpdate(playerWeapon, computerWeapon) {
+    if (playerWeapon){
+        computerChoiceOutput.innerHTML = `Computer choice: ${computerWeapon}` 
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Modal for help button
 
 //Variable declarations
