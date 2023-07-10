1579bc49ec24a879b428a44202a43dd51e98f696
MattuW4 <>
2023-07-10 11:19:25 +0000

    Add modal to html and css style. Re-add JS script after accidental deletion. Automate modal with JS.

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+// Modal for help button
+
+//Variable declarations
+const modal = document.getElementById("my-modal");
+const openModal = document.getElementById("open-modal-button");
+const closeModal = document.querySelector(".return-button");
+
+/**  Modal open and hide */
+openModal.addEventListener("click", showModal);
+closeModal.addEventListener("click", hideModal);
+
+/** Show modal function */
+function showModal() {
+    modal.style.display = "block";
+}
+
+/** Hide modal function */
+function hideModal() {
+    modal.style.display = "none";
+}
